37ae238037b73034600729b88c47584e10fe118a
techno-pixel <jseaidoun@gmail.com>
2021-01-26 10:15:54 -0500

    Finished lab2

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
